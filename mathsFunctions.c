38c776d75a3e72ab3f32e59a5e05d7c9de4bcac2
Sean <p16187449@my365.dmu.ac.uk>
2018-01-23 14:36:04 +0000

    Merging Master and Testing

--- mathsFunctions.c
+++ mathsFunctions.c
@@ -1,8 +1,12 @@
 #include <stdhi.o>
 
 int main(){
+<<<<<<< HEAD
 	
 // Comment from Master
+=======
+	// I am branch testing
+>>>>>>> testing
 	int x = 5, y = 6;
 	printf ("%i + %i = %i\n", x, y, x+y);
 
