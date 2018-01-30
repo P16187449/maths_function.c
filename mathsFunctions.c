83c8af635f761e03eb958d1df14914b65660049c
Sean <p16187449@my365.dmu.ac.uk>
2018-01-30 14:21:54 +0000

    Added min/max function (accepts 5 integers)

--- mathsFunctions.c
+++ mathsFunctions.c
@@ -12,5 +12,15 @@ int main(){
 	printf ("%i + %i = %i\n", x, y, x/y);
 
 	printf ("%i MOD %i = %i\n", x, y, x%y);
+
+	int max, min;
+	int nums[];
+	for(int i = 0; i < 5; i++){
+		if(nums[i] < min){
+			min = nums[i];
+		} else if(nums[i] > max){
+			max = nums[i];
+		}
+	}
 	return 0;
 }
